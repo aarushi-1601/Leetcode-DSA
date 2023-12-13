89d58de58820bc373d587e69bddf1179ff9092aa
Aarushi Jain <92649573+aarushi-1601@users.noreply.github.com>
2023-12-13 18:17:14 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (14.92%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x==0) return 0 ;
+        int s=1, e=x/2 ;
+        long long int mid = s + (e-s)/2 ;
+        while (s<=e){
+            if ((mid*mid)<=x && (mid+1)*(mid+1)>x){
+                return mid ;
+            }
+            if (mid*mid > x){
+                e = mid-1 ;
+            }
+            else s = mid+1 ;
+            mid = s + (e-s)/2 ;
+        }
+        return 1 ;
+    }
+};
