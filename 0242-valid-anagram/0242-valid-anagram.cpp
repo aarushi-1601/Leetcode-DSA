cf1fbcf085575abd0bf39ce1b0685d77ff47c3df
Aarushi Jain <92649573+aarushi-1601@users.noreply.github.com>
2023-12-24 21:05:11 +0530

    Time: 8 ms (59.14%), Space: 7.6 MB (78.77%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> freq(string& s){
+        vector<int> ans(26, 0);
+        for(char c: s){
+            ans[c-'a']++;
+        }
+        return ans;
+    }
+    bool isAnagram(string s, string t) {
+        return freq(s)==freq(t);
+    }
+};
